3e7496be6c1f3a21d79e3bb6e59aeac63a10c7c9
Koichi5 <koichi20021217@gmail.com>
2023-09-29 01:44:52 +0900

    delete home page widget

--- lib/main.dart
+++ lib/main.dart
@@ -20,14 +20,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("hello")),
-    );
-  }
-}
