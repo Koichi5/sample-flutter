617d581b6b958d7d4a6956d602ce2c6d173dedf8
Koichi5 <koichi20021217@gmail.com>
2024-02-10 00:56:47 +0900

    add photo view

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sample_flutter/photo_view/view/photo_view_sample.dart';
 
 void main() {
   runApp(
@@ -19,6 +20,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      home: const PhotoViewSample(),
     );
   }
 }
