ad3dcb0abc650e921d4e71a702a6508e6c2ffd85
Koichi5 <koichi20021217@gmail.com>
2024-02-19 14:57:30 +0900

    add webview

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sample_flutter/webview_flutter/views/webview_flutter_sample.dart';
 
 void main() {
   runApp(
@@ -13,8 +14,9 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
+      home: WebviewFlutterSample(),
     );
   }
 }
