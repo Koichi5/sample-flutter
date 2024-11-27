31413f65f8b4b7752289ec5511ee471f0ec7b8c0
Koichi5 <koichi20021217@gmail.com>
2024-11-28 02:48:27 +0900

    fix: main.dart の変更

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:sample_flutter/async_queue/async_queue_sample.dart';
-import 'package:sample_flutter/font_sample/font_sample_screen.dart';
-import 'package:sample_flutter/font_sample/slack_sample_screen.dart';
-import 'package:sample_flutter/spacing/spacing_screen.dart';
-import 'package:sample_flutter/views/hello_world_screen/hello_world_screen.dart';
 
 void main() {
-  debugProfileBuildsEnabled = true;
-  debugProfileBuildsEnabledUserWidgets = true;
-  debugProfileLayoutsEnabled = true;
-  debugProfilePaintsEnabled = true;
   runApp(
     const ProviderScope(
       child: MyApp(),
@@ -30,33 +20,7 @@ class MyApp extends StatelessWidget {
         fontFamily: 'Lato',
         fontFamilyFallback: const ['NotoSans'],
       ),
-      home: const AsyncQueueSample(),
+      home: const Placeholder(),
     );
-  }
-}
-
-class WidgetCache<T> extends StatefulWidget {
-  const WidgetCache({super.key, required this.value, required this.builder});
-
-  final T value;
-  final Widget Function(BuildContext context, T value) builder;
-
-  @override
-  WidgetCacheState<T> createState() => WidgetCacheState<T>();
-}
-
-class WidgetCacheState<T> extends State<WidgetCache<T>> {
-  late Widget cache;
-  T? previousValue;
-
-  @override
-  Widget build(BuildContext context) {
-    if (identical(widget.value, previousValue) == false) {
-      previousValue = widget.value;
-      cache = Builder(
-        builder: (context) => widget.builder(context, widget.value),
-      );
-    }
-    return cache;
   }
 }
